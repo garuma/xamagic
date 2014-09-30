e8601e8685ab38e83c5ba0cfcdc0d7f42cc7ea28
Jérémie Laval <jeremie.laval@gmail.com>
2014-09-29 21:51:20 -0400

    Move some curves to Tau

--- MagicalKingdom/Experiments/AnticipationContentFragment.cs
+++ MagicalKingdom/Experiments/AnticipationContentFragment.cs
@@ -85,23 +85,5 @@ namespace MagicalKingdom
 			}
 		}
 	}
-
-	class AnticipationCurve : ITimeCurve
-	{
-		const double CutOff = 2/3.0;
-		InvertedQuadraticCurve secondCurve = new InvertedQuadraticCurve ();
-
-		public double Interpolate (double time)
-		{
-			if (time < CutOff) {
-				// The first part of the curve is a sine wave correctly scaled and offset
-				return Math.Sin (time * 3 * Math.PI) / (8 * (1 - time));
-			} else {
-				// For the second part we delegate to a normal inverted quadratic curve with a readjusted time
-				var adjustedTime = (time - CutOff) * 3;
-				return secondCurve.Interpolate (adjustedTime);
-			}
-		}
-	}
 }
 
--- MagicalKingdom/Experiments/SquashStretchContentFragment.cs
+++ MagicalKingdom/Experiments/SquashStretchContentFragment.cs
@@ -85,7 +85,7 @@ namespace MagicalKingdom
 
 		public ITimeCurve CreatePreferredTimeCurve ()
 		{
-			return new CriticalSpringDamperCurve ();
+			return new DoubleCriticalSpringDamperCurve ();
 		}
 
 		public void SetTimeCurve (ITimeCurve curve)
@@ -96,25 +96,6 @@ namespace MagicalKingdom
 		public string PreferredCurveName {
 			get {
 				return "Squash/Stretch";
-			}
-		}
-
-		class CriticalSpringDamperCurve : ITimeCurve
-		{
-			public double Interpolate (double time)
-			{
-				if (time <= 0.5) {
-					var adjusted = time * 2;
-					return (2.0 / (ExponentialDecay (adjusted, 3) + 1)) - 1;
-				} else {
-					var adjusted = (time - 0.5) * 2;
-					return ExponentialDecay (adjusted, 2);
-				}
-			}
-
-			double ExponentialDecay (double input, double ratio)
-			{
-				return (1 - input) * Math.Exp (-ratio * input);
 			}
 		}
 	}
